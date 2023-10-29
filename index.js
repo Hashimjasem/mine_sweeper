3334165bc02c126df6d59837c05514bf4abe5045
Hashimjasem <j_hashim1305@outlook.com>
2023-10-30 05:36:56 +0800

    hour 6

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
 createTiles();
 
-// tiles.forEach((tile, index) => {
-//   const surroundingTiles = [
-//     //above
-//     index - 11,
-//     index - 10,
-//     index - 9,
-  
-//     //either side
-//     index - 1,
-//     index + 1,
-    
-//     //below
-//     index + 9,
-//     index + 10,
-//     index + 11,
-//   ];
-//   tile.addEventListener("click", () => {
-//     console.log("Clicked tile index:", index);
-//     console.log("surrounding tiles:", surroundingTiles)
-//   });
-// });
-
 function createTiles() {
   const tiles = [];
+
+  //creates tiles and pushes them into tiles array
   for (let i = 0; i < 100; i++) {
     const tile = document.createElement("div");
     tile.classList.add("tile");
@@ -31,38 +11,42 @@ function createTiles() {
     grid.appendChild(tile);
 
     tiles.push(tile);
+
   }
 
   tiles.forEach((tile) => {
-    int = Math.random(0,1)
-    if(int >= 0.90){
+    int = Math.random(0, 1)
+    if (int >= 0.90) {
       tile.classList.add("red");
     }
   });
 
   tiles.forEach((tile, index) => {
-    const surroundingTiles = [
-      //above
-      index - 11,
-      index - 10,
-      index - 9,
-    
-      //either side
-      index - 1,
-      index + 1,
-      
-      //below
-      index + 9,
-      index + 10,
-      index + 11,
-    ];
+
 
 
 
     tile.addEventListener("click", () => {
+      const surroundingTiles = [
+        //above
+        index - 11,
+        index - 10,
+        index - 9,
+  
+        //either side
+        index - 1,
+        index + 1,
+  
+        //below
+        index + 9,
+        index + 10,
+        index + 11,
+      ];
+
       console.log("Clicked tile index:", index);
       console.log("surrounding tiles:", surroundingTiles)
-      if(tile.classList.contains('red')){
+
+      if (tile.classList.contains('red')) {
         console.log('mine')
       }
     });
