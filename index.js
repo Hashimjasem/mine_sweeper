4d4acca69abda05396e2636bc9389c6d39f8db1d
Hashimjasem <j_hashim1305@outlook.com>
2023-10-30 04:27:46 +0800

    random mine spawn

--- index.js
+++ index.js
@@ -1,39 +1,69 @@
-const tiles = createTiles();
+createTiles();
 
-tiles.forEach((tile, index) => {
-  const surroundingTiles = [
-    //above
-    index - 11,
-    index - 10,
-    index - 9,
+// tiles.forEach((tile, index) => {
+//   const surroundingTiles = [
+//     //above
+//     index - 11,
+//     index - 10,
+//     index - 9,
   
-    //either side
-    index - 1,
-    index + 1,
+//     //either side
+//     index - 1,
+//     index + 1,
     
-    //below
-    index + 9,
-    index + 10,
-    index + 11,
-  ];
-  tile.addEventListener("click", () => {
-    console.log("Clicked tile index:", index);
-    console.log("surrounding tiles:", surroundingTiles)
-  });
-});
+//     //below
+//     index + 9,
+//     index + 10,
+//     index + 11,
+//   ];
+//   tile.addEventListener("click", () => {
+//     console.log("Clicked tile index:", index);
+//     console.log("surrounding tiles:", surroundingTiles)
+//   });
+// });
 
 function createTiles() {
   const tiles = [];
-
   for (let i = 0; i < 100; i++) {
     const tile = document.createElement("div");
     tile.classList.add("tile");
-
     const grid = document.querySelector(".grid");
     grid.appendChild(tile);
 
     tiles.push(tile);
   }
 
+  tiles.forEach((tile) => {
+    int = Math.random(0,1)
+    if(int >= 0.9){
+      tile.classList.add("red");
+    }
+  });
+
+  tiles.forEach((tile, index) => {
+    const surroundingTiles = [
+      //above
+      index - 11,
+      index - 10,
+      index - 9,
+    
+      //either side
+      index - 1,
+      index + 1,
+      
+      //below
+      index + 9,
+      index + 10,
+      index + 11,
+    ];
+
+
+    tile.addEventListener("click", () => {
+      console.log("Clicked tile index:", index);
+      console.log("surrounding tiles:", surroundingTiles)
+    });
+  });
+
   return tiles;
 }
+
