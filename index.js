10444fe3889acb49663d388a258e0cb02ff9c672
Hashimjasem <j_hashim1305@outlook.com>
2023-10-30 02:54:55 +0800

    this is beautiful

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const tiles = createTiles();
 
 tiles.forEach((tile, index) => {
+  const surroundingTiles = [
+    //above
+    index - 11,
+    index - 10,
+    index - 9,
+  
+    //either side
+    index - 1,
+    index + 1,
+    
+    //below
+    index + 9,
+    index + 10,
+    index + 11,
+  ];
   tile.addEventListener("click", () => {
     console.log("Clicked tile index:", index);
+    console.log("surrounding tiles:", surroundingTiles)
   });
 });
 
