10d4e724eda8a39a20b95e537f4aa78ae48c4da2
Hashimjasem <j_hashim1305@outlook.com>
2023-10-30 01:46:36 +0800

    now click return the index

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
+const tiles = createTiles();
+
+tiles.forEach((tile, index) => {
+  tile.addEventListener("click", () => {
+    console.log("Clicked tile index:", index);
+  });
+});
+
 function createTiles() {
-    // Create an empty array to store the tiles
-    const tiles = [];
-  
-    // Create a for loop to run 100 times
-    for (let i = 0; i < 100; i++) {
-      // Create a new tile
-      const tile = document.createElement("div");
-      tile.classList.add("tile");
-  
-      // Add the tile to the grid div
-      const grid = document.querySelector(".grid");
-      grid.appendChild(tile);
-  
-      // Add the tile to the tiles array
-      tiles.push(tile);
-    }
-  
-    // Return the array of tiles
-    console.log(tiles)
-    tiles.forEach((tile) => {
-        tile.addEventListener("click", () => {
-          // Do something when the tile is clicked
-          tile.classList.add("red");
-        });
-      });
-      
-    return tiles;
-    
+  const tiles = [];
+
+  for (let i = 0; i < 100; i++) {
+    const tile = document.createElement("div");
+    tile.classList.add("tile");
+
+    const grid = document.querySelector(".grid");
+    grid.appendChild(tile);
+
+    tiles.push(tile);
   }
-  // Run the function on page load
-  window.addEventListener("load", createTiles);
+
+  return tiles;
+}
